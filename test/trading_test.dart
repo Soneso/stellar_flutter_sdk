fc3e2d74145886810834d5a3f6e77ac2e914e22e
Christian Rogobete <c.rogobete@soneso.com>
2020-06-25 23:53:08 +0200

    add create passive sell offer test

--- test/trading_test.dart
+++ test/trading_test.dart
@@ -193,4 +193,105 @@ void main() {
 
   });
 
+  test('create passive sell offer', () async {
+
+    KeyPair issuerKeipair = KeyPair.random();
+    KeyPair sellerKeipair = KeyPair.random();
+
+    String issuerAccountId = issuerKeipair.accountId;
+    String sellerAccountId = sellerKeipair.accountId;
+
+    await FriendBot.fundTestAccount(sellerAccountId);
+
+    AccountResponse sellerAccount = await sdk.accounts.account(sellerAccountId);
+    CreateAccountOperation co = CreateAccountOperationBuilder(issuerAccountId, "10").build();
+    Transaction transaction = TransactionBuilder(sellerAccount, Network.TESTNET).addOperation(co).build();
+    transaction.sign(sellerKeipair);
+    SubmitTransactionResponse response = await sdk.submitTransaction(transaction);
+    assert(response.success);
+
+    AccountResponse issuerAccount = await sdk.accounts.account(issuerAccountId);
+
+    Asset marsDollar = AssetTypeCreditAlphaNum4("MARS", issuerAccountId);
+
+    ChangeTrustOperationBuilder ctob = ChangeTrustOperationBuilder(marsDollar,"10000");
+    transaction = TransactionBuilder(sellerAccount, Network.TESTNET).addOperation(ctob.build()).build();
+    transaction.sign(sellerKeipair);
+
+    response = await sdk.submitTransaction(transaction);
+    assert(response.success);
+
+    PaymentOperation po = PaymentOperationBuilder(sellerAccountId, marsDollar, "2000").build();
+    transaction = TransactionBuilder(issuerAccount, Network.TESTNET).addOperation(po).build();
+    transaction.sign(issuerKeipair);
+
+    response = await sdk.submitTransaction(transaction);
+    assert(response.success);
+
+    String amountSelling = "100";
+    String price = "0.5";
+
+    CreatePassiveSellOfferOperation cpso = CreatePassiveSellOfferOperationBuilder(marsDollar, Asset.NATIVE, amountSelling, price).build();
+    transaction = TransactionBuilder(sellerAccount, Network.TESTNET).addOperation(cpso).build();
+    transaction.sign(sellerKeipair);
+    response = await sdk.submitTransaction(transaction);
+    assert(response.success);
+
+    List<OfferResponse> offers = (await sdk.offers.forAccount(sellerAccountId).execute()).records;
+    assert(offers.length == 1);
+    OfferResponse offer = offers.first;
+    assert(offer.buying == Asset.NATIVE);
+    assert(offer.selling == marsDollar);
+
+    double offerAmount = double.parse(offer.amount);
+    double sellingAmount = double.parse(amountSelling);
+    assert(offerAmount == sellingAmount);
+
+    double offerPrice = double.parse(offer.price);
+    double sellingPrice = double.parse(price);
+    assert(offerPrice == sellingPrice);
+
+    assert(offer.seller.accountId == sellerAccountId);
+
+    int offerId = offer.id;
+
+    // update offer
+    amountSelling = "150";
+    price = "0.3";
+    ManageSellOfferOperation ms = ManageSellOfferOperationBuilder(marsDollar, Asset.NATIVE, amountSelling, price).setOfferId(offerId).build();
+    transaction = TransactionBuilder(sellerAccount, Network.TESTNET).addOperation(ms).build();
+    transaction.sign(sellerKeipair);
+    response = await sdk.submitTransaction(transaction);
+    assert(response.success);
+
+    offers = (await sdk.offers.forAccount(sellerAccountId).execute()).records;
+    assert(offers.length == 1);
+    offer = offers.first;
+    assert(offer.buying == Asset.NATIVE);
+    assert(offer.selling == marsDollar);
+
+    offerAmount = double.parse(offer.amount);
+    sellingAmount = double.parse(amountSelling);
+    assert(offerAmount == sellingAmount);
+
+    offerPrice = double.parse(offer.price);
+    sellingPrice = double.parse(price);
+
+    assert(offerPrice == sellingPrice);
+
+    assert(offer.seller.accountId == sellerAccountId);
+
+    // delete offer
+    amountSelling = "0";
+    ms = ManageSellOfferOperationBuilder(marsDollar, Asset.NATIVE, amountSelling, price).setOfferId(offerId).build();
+    transaction = TransactionBuilder(sellerAccount, Network.TESTNET).addOperation(ms).build();
+    transaction.sign(sellerKeipair);
+    response = await sdk.submitTransaction(transaction);
+    assert(response.success);
+
+    offers = (await sdk.offers.forAccount(sellerAccountId).execute()).records;
+    assert(offers.length == 0);
+
+  });
+
 }
