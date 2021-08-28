89e5e609e9d25cd790dc851cb0e09714190fed96
hasToDev <hasdikus@gmail.com>
2021-08-28 20:18:44 +0700

    bugfix sep0011_test.dart

--- test/sep0011_test.dart
+++ test/sep0011_test.dart
@@ -124,10 +124,8 @@ signatures.len: 1
 signatures[0].hint: b51d604e
 signatures[0].signature: c52a9c15a60a9b7281cb9e932e0eb1ffbe9a759b6cc242eeb08dda88cfff3faaa47b5d817153617825941d1d0c46523f54d9b3790f1cee1370af08a5c29dfe03''';
 
-    String transactionEnvelopeXdrBase64 =
-        TxRep.transactionEnvelopeXdrBase64FromTxRep(txRep);
-    String txRepRes =
-        TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
+    String transactionEnvelopeXdrBase64 = TxRep.transactionEnvelopeXdrBase64FromTxRep(txRep);
+    String txRepRes = TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
     print(txRepRes);
     assert(txRepRes == txRep);
   });
@@ -256,8 +254,7 @@ feeBump.signatures.len: 1
 feeBump.signatures[0].hint: 7b21e7e3
 feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e99c8aa2e9cdcbf7b8b24872e129e645501dbddb427d400fa92af69768fe62a80b041d0efefa5fc90a''';
 
-    String transactionEnvelopeXdrBase64 =
-        TxRep.transactionEnvelopeXdrBase64FromTxRep(txRep);
+    String transactionEnvelopeXdrBase64 = TxRep.transactionEnvelopeXdrBase64FromTxRep(txRep);
     String xdr =
         'AAAABQAAAABHxVp7PehpcAKmckXj36JutxZ5A+PRz7BDAu8G/dluWQAAAAAAAAXrAAAAAgAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAABXgAAAEAyhakEgAAAAEAAAAAXxYTwAAAAABfFhogAAAAAQAAABZFbmpveSB0aGlzIHRyYW5zYWN0aW9uAAAAAAAOAAAAAAAAAAAAAAAAWuWjUwjUN29Tinf8R9pUL5cd9Dg7Vlor7vgPi45MWrt//////////wAAAAEAAAAA7rUTHZx4gZK/Lm2Cm8wHma5gYr9ORsPjmSQSa3sh5+MAAAABAAAAAFrlo1MI1DdvU4p3/EfaVC+XHfQ4O1ZaK+74D4uOTFq7AAAAAH//////////AAAAAQAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAAAAEAAAAAWuWjUwjUN29Tinf8R9pUL5cd9Dg7Vlor7vgPi45MWrsAAAABVVNEAAAAAAAyUlQyIZKfbs+tUWuvK7N0nGSCII0/Go1/CpHXNW3tC3//////////AAAAAAAAAAIAAAABSU9NAAAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAAAAABMS0AAAAAAFrlo1MI1DdvU4p3/EfaVC+XHfQ4O1ZaK+74D4uOTFq7AAAAAU1PT04AAAAA7rUTHZx4gZK/Lm2Cm8wHma5gYr9ORsPjmSQSa3sh5+MAAAAABMS0AAAAAAIAAAABRUNPAAAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAAAAJBU1RSTwAAAAAAAAAAAAAA7rUTHZx4gZK/Lm2Cm8wHma5gYr9ORsPjmSQSa3sh5+MAAAAAAAAADQAAAAFJT00AAAAAAO61Ex2ceIGSvy5tgpvMB5muYGK/TkbD45kkEmt7IefjAAAAAO5rKAAAAAAAWuWjUwjUN29Tinf8R9pUL5cd9Dg7Vlor7vgPi45MWrsAAAABTU9PTgAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAAAALLQXgAAAAAAgAAAAFFQ08AAAAAAO61Ex2ceIGSvy5tgpvMB5muYGK/TkbD45kkEmt7IefjAAAAAkFTVFJPAAAAAAAAAAAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAAAAAAAAAFAAAAAQAAAABa5aNTCNQ3b1OKd/xH2lQvlx30ODtWWivu+A+LjkxauwAAAAEAAAACAAAAAQAAAAQAAAABAAAAegAAAAEAAAAKAAAAAQAAADIAAAABAAAAegAAAAEAAAAeaHR0cHM6Ly93d3cuc29uZXNvLmNvbS9ibHViYmVyAAAAAAABAAAAAFrlo1MI1DdvU4p3/EfaVC+XHfQ4O1ZaK+74D4uOTFq7AAAAMgAAAAAAAAADAAAAAUVDTwAAAAAA7rUTHZx4gZK/Lm2Cm8wHma5gYr9ORsPjmSQSa3sh5+MAAAAAAAAAE0h06QAAAAAHAAAACgAACHTtxeZ9AAAAAAAAAAQAAAACQVNUUk8AAAAAAAAAAAAAAO61Ex2ceIGSvy5tgpvMB5muYGK/TkbD45kkEmt7IefjAAAAAU1PT04AAAAA7rUTHZx4gZK/Lm2Cm8wHma5gYr9ORsPjmSQSa3sh5+MAAAAGlZ6OAAAAAAEAAAACAAAAAAAAAAYAAAACQVNUUk8AAAAAAAAAAAAAAO61Ex2ceIGSvy5tgpvMB5muYGK/TkbD45kkEmt7IefjAAAAF0h26AAAAAAAAAAABwAAAABa5aNTCNQ3b1OKd/xH2lQvlx30ODtWWivu+A+LjkxauwAAAAFNT09OAAAAAQAAAAAAAAAIAAAAAFrlo1MI1DdvU4p3/EfaVC+XHfQ4O1ZaK+74D4uOTFq7AAAAAAAAAAoAAAAGU29tbWVyAAAAAAABAAAAFERpZSBN9mJlbCBzaW5kIGhlad8hAAAAAAAAAAsAAAEAyhakGwAAAAAAAAAMAAAAAU1PT04AAAAA7rUTHZx4gZK/Lm2Cm8wHma5gYr9ORsPjmSQSa3sh5+MAAAABRUNPAAAAAADutRMdnHiBkr8ubYKbzAeZrmBiv05Gw+OZJBJreyHn4wAAAAAHJw4AAAAAAQAAAAUAAAh07cXmfgAAAAAAAAABeyHn4wAAAEAIWi7mG+DVvCwsfH6QzEySH+v+JapUtumciqLpzcv3uLJIcuEp5kVQHb3bQn1AD6kq9pdo/mKoCwQdDv76X8kKAAAAAAAAAAF7IefjAAAAQAhaLuYb4NW8LCx8fpDMTJIf6/4lqlS26ZyKounNy/e4skhy4SnmRVAdvdtCfUAPqSr2l2j+YqgLBB0O/vpfyQo=';
     assert(xdr == transactionEnvelopeXdrBase64);
@@ -387,13 +384,134 @@ feeBump.signatures.len: 1
 feeBump.signatures[0].hint: 7b21e7e3 (bad signature/unknown key/main is wrong network)
 feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e99c8aa2e9cdcbf7b8b24872e129e645501dbddb427d400fa92af69768fe62a80b041d0efefa5fc90a''';
 
-    String transactionEnvelopeXdrBase64 =
-        TxRep.transactionEnvelopeXdrBase64FromTxRep(txRep);
+    String txRepWithoutComments = '''
+type: ENVELOPE_TYPE_TX_FEE_BUMP
+feeBump.tx.feeSource: GBD4KWT3HXUGS4ACUZZELY67UJXLOFTZAPR5DT5QIMBO6BX53FXFSLQS
+feeBump.tx.fee: 1515
+feeBump.tx.innerTx.type: ENVELOPE_TYPE_TX
+feeBump.tx.innerTx.tx.sourceAccount: GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.fee: 1400
+feeBump.tx.innerTx.tx.seqNum: 1102902109202
+feeBump.tx.innerTx.tx.timeBounds._present: true
+feeBump.tx.innerTx.tx.timeBounds.minTime: 1595282368
+feeBump.tx.innerTx.tx.timeBounds.maxTime: 1595284000
+feeBump.tx.innerTx.tx.memo.type: MEMO_TEXT
+feeBump.tx.innerTx.tx.memo.text: "Enjoy this transaction"
+feeBump.tx.innerTx.tx.operations.len: 14
+feeBump.tx.innerTx.tx.operations[0].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[0].body.type: CREATE_ACCOUNT
+feeBump.tx.innerTx.tx.operations[0].body.createAccountOp.destination: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[0].body.createAccountOp.startingBalance: 9223372036854775807
+feeBump.tx.innerTx.tx.operations[1].sourceAccount._present: true
+feeBump.tx.innerTx.tx.operations[1].sourceAccount: GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[1].body.type: PAYMENT
+feeBump.tx.innerTx.tx.operations[1].body.paymentOp.destination: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[1].body.paymentOp.asset: XLM
+feeBump.tx.innerTx.tx.operations[1].body.paymentOp.amount: 9223372036854775807
+feeBump.tx.innerTx.tx.operations[2].sourceAccount._present: true
+feeBump.tx.innerTx.tx.operations[2].sourceAccount: GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[2].body.type: PAYMENT
+feeBump.tx.innerTx.tx.operations[2].body.paymentOp.destination: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[2].body.paymentOp.asset: USD:GAZFEVBSEGJJ63WPVVIWXLZLWN2JYZECECGT6GUNP4FJDVZVNXWQWMYI
+feeBump.tx.innerTx.tx.operations[2].body.paymentOp.amount: 9223372036854775807
+feeBump.tx.innerTx.tx.operations[3].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[3].body.type: PATH_PAYMENT_STRICT_RECEIVE
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.sendAsset: IOM:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.sendMax: 20000000
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.destination: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.destAsset: MOON:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.destAmount: 80000000
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.path.len: 2
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.path[0]: ECO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[3].body.pathPaymentStrictReceiveOp.path[1]: ASTRO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[4].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[4].body.type: PATH_PAYMENT_STRICT_SEND
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.sendAsset: IOM:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.sendAmount: 4000000000
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.destination: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.destAsset: MOON:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.destMin: 12000000000
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.path.len: 2
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.path[0]: ECO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[4].body.pathPaymentStrictSendOp.path[1]: ASTRO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[5].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[5].body.type: SET_OPTIONS
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.inflationDest._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.inflationDest: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.clearFlags._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.clearFlags: 2
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.setFlags._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.setFlags: 4
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.masterWeight._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.masterWeight: 122
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.lowThreshold._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.lowThreshold: 10
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.medThreshold._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.medThreshold: 50
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.highThreshold._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.highThreshold: 122
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.homeDomain._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.homeDomain: "https://www.soneso.com/blubber"
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.signer._present: true
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.signer.key: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[5].body.setOptionsOp.signer.weight: 50
+feeBump.tx.innerTx.tx.operations[6].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[6].body.type: MANAGE_SELL_OFFER
+feeBump.tx.innerTx.tx.operations[6].body.manageSellOfferOp.selling: ECO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[6].body.manageSellOfferOp.buying: XLM
+feeBump.tx.innerTx.tx.operations[6].body.manageSellOfferOp.amount: 82820000000
+feeBump.tx.innerTx.tx.operations[6].body.manageSellOfferOp.price.n: 7
+feeBump.tx.innerTx.tx.operations[6].body.manageSellOfferOp.price.d: 10
+feeBump.tx.innerTx.tx.operations[6].body.manageSellOfferOp.offerID: 9298298398333
+feeBump.tx.innerTx.tx.operations[7].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[7].body.type: CREATE_PASSIVE_SELL_OFFER
+feeBump.tx.innerTx.tx.operations[7].body.createPassiveSellOfferOp.selling: ASTRO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[7].body.createPassiveSellOfferOp.buying: MOON:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[7].body.createPassiveSellOfferOp.amount: 28280000000
+feeBump.tx.innerTx.tx.operations[7].body.createPassiveSellOfferOp.price.n: 1
+feeBump.tx.innerTx.tx.operations[7].body.createPassiveSellOfferOp.price.d: 2
+feeBump.tx.innerTx.tx.operations[8].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[8].body.type: CHANGE_TRUST
+feeBump.tx.innerTx.tx.operations[8].body.changeTrustOp.line: ASTRO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[8].body.changeTrustOp.limit: 100000000000
+feeBump.tx.innerTx.tx.operations[9].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[9].body.type: ALLOW_TRUST
+feeBump.tx.innerTx.tx.operations[9].body.allowTrustOp.trustor: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[9].body.allowTrustOp.asset: MOON
+feeBump.tx.innerTx.tx.operations[9].body.allowTrustOp.authorize: 1
+feeBump.tx.innerTx.tx.operations[10].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[10].body.type: ACCOUNT_MERGE
+feeBump.tx.innerTx.tx.operations[10].body.destination: GBNOLI2TBDKDO32TRJ37YR62KQXZOHPUHA5VMWRL534A7C4OJRNLWOJP
+feeBump.tx.innerTx.tx.operations[11].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[11].body.type: MANAGE_DATA
+feeBump.tx.innerTx.tx.operations[11].body.manageDataOp.dataName: "Sommer"
+feeBump.tx.innerTx.tx.operations[11].body.manageDataOp.dataValue._present: true
+feeBump.tx.innerTx.tx.operations[11].body.manageDataOp.dataValue: 446965204df662656c2073696e6420686569df21
+feeBump.tx.innerTx.tx.operations[12].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[12].body.type: BUMP_SEQUENCE
+feeBump.tx.innerTx.tx.operations[12].body.bumpSequenceOp.bumpTo: 1102902109211
+feeBump.tx.innerTx.tx.operations[13].sourceAccount._present: false
+feeBump.tx.innerTx.tx.operations[13].body.type: MANAGE_BUY_OFFER
+feeBump.tx.innerTx.tx.operations[13].body.manageBuyOfferOp.selling: MOON:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[13].body.manageBuyOfferOp.buying: ECO:GDXLKEY5TR4IDEV7FZWYFG6MA6M24YDCX5HENQ7DTESBE233EHT6HHGK
+feeBump.tx.innerTx.tx.operations[13].body.manageBuyOfferOp.buyAmount: 120000000
+feeBump.tx.innerTx.tx.operations[13].body.manageBuyOfferOp.price.n: 1
+feeBump.tx.innerTx.tx.operations[13].body.manageBuyOfferOp.price.d: 5
+feeBump.tx.innerTx.tx.operations[13].body.manageBuyOfferOp.offerID: 9298298398334
+feeBump.tx.innerTx.tx.ext.v: 0
+feeBump.tx.innerTx.signatures.len: 1
+feeBump.tx.innerTx.signatures[0].hint: 7b21e7e3
+feeBump.tx.innerTx.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e99c8aa2e9cdcbf7b8b24872e129e645501dbddb427d400fa92af69768fe62a80b041d0efefa5fc90a
+feeBump.tx.ext.v: 0
+feeBump.signatures.len: 1
+feeBump.signatures[0].hint: 7b21e7e3
+feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e99c8aa2e9cdcbf7b8b24872e129e645501dbddb427d400fa92af69768fe62a80b041d0efefa5fc90a''';
+
+    String transactionEnvelopeXdrBase64 = TxRep.transactionEnvelopeXdrBase64FromTxRep(txRep);
     print(transactionEnvelopeXdrBase64);
-    String txRepRes =
-        TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
+    String txRepRes = TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
     print(txRepRes);
-    assert(txRepRes == txRep);
+    assert(txRepRes == txRepWithoutComments);
   });
 
   test('xdr test', () {
@@ -417,16 +535,14 @@ feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e
     Operation createAccount = new CreateAccountOperationBuilder(
             accountBId, "2200000000000000000020111229198190202020202.1230019")
         .build();
-    Operation payment = new PaymentOperationBuilder(accountBId, Asset.NATIVE,
-            "3333333000000000000020111229198190202020202.123333")
+    Operation payment = new PaymentOperationBuilder(
+            accountBId, Asset.NATIVE, "3333333000000000000020111229198190202020202.123333")
         .setSourceAccount(accountAId)
         .build();
-    Asset nonNativeAsset = AssetTypeCreditAlphaNum4(
-        "USD", "GAZFEVBSEGJJ63WPVVIWXLZLWN2JYZECECGT6GUNP4FJDVZVNXWQWMYI");
+    Asset nonNativeAsset =
+        AssetTypeCreditAlphaNum4("USD", "GAZFEVBSEGJJ63WPVVIWXLZLWN2JYZECECGT6GUNP4FJDVZVNXWQWMYI");
     Operation nonNativePayment = new PaymentOperationBuilder(
-            accountBId,
-            nonNativeAsset,
-            "3333333000000000000020111229198190202020202.123333")
+            accountBId, nonNativeAsset, "3333333000000000000020111229198190202020202.123333")
         .setSourceAccount(accountAId)
         .build();
 
@@ -436,14 +552,12 @@ feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e
     Asset moonAsset = AssetTypeCreditAlphaNum4("MOON", keyPairA.accountId);
     List<Asset> path = [ecoAsset, astroAsset];
     PathPaymentStrictReceiveOperation strictReceive =
-        PathPaymentStrictReceiveOperationBuilder(
-                iomAsset, "2", accountBId, moonAsset, "8")
+        PathPaymentStrictReceiveOperationBuilder(iomAsset, "2", accountBId, moonAsset, "8")
             .setPath(path)
             .build();
 
     PathPaymentStrictSendOperation strictSend =
-        PathPaymentStrictSendOperationBuilder(
-                iomAsset, "400", accountBId, moonAsset, "1200")
+        PathPaymentStrictSendOperationBuilder(iomAsset, "400", accountBId, moonAsset, "1200")
             .setPath(path)
             .build();
 
@@ -457,9 +571,7 @@ feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e
             .build();
 
     CreatePassiveSellOfferOperation createPassiveSellOfferOperation =
-        CreatePassiveSellOfferOperationBuilder(
-                astroAsset, moonAsset, "2828", '0.5')
-            .build();
+        CreatePassiveSellOfferOperationBuilder(astroAsset, moonAsset, "2828", '0.5').build();
 
     String limit = "10000";
     ChangeTrustOperation changeTrustOperation =
@@ -474,7 +586,7 @@ feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e
     XdrSignerKey signer = XdrSignerKey();
     signer.discriminant = XdrSignerKeyType.SIGNER_KEY_TYPE_ED25519;
     signer.ed25519 = XdrUint256();
-    signer.ed25519.uint256 = StrKey.decodeStellarAccountId(accountBId);
+    signer.ed25519!.uint256 = StrKey.decodeStellarAccountId(accountBId);
 
     SetOptionsOperation setOptionsOperation = SetOptionsOperationBuilder()
         .setInflationDestination(accountBId)
@@ -499,11 +611,10 @@ feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e
     List<int> list = value.codeUnits;
     Uint8List valueBytes = Uint8List.fromList(list);
 
-    ManageDataOperation manageDataOperation =
-        ManageDataOperationBuilder(key, valueBytes).build();
+    ManageDataOperation manageDataOperation = ManageDataOperationBuilder(key, valueBytes).build();
 
     BumpSequenceOperation bumpSequenceOperation =
-        BumpSequenceOperationBuilder(a.sequenceNumber + 10).build();
+        BumpSequenceOperationBuilder(a.sequenceNumber! + 10).build();
 
     Transaction transaction = new TransactionBuilder(a)
         .addTimeBounds(tb)
@@ -534,14 +645,12 @@ feeBump.signatures[0].signature: 085a2ee61be0d5bc2c2c7c7e90cc4c921febfe25aa54b6e
     feeBump.sign(keyPairC, Network.TESTNET);
     String transactionEnvelopeXdrBase64 = feeBump.toEnvelopeXdrBase64();
     print(transactionEnvelopeXdrBase64);
-    String txrep =
-        TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
+    String txrep = TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
     print(txrep);
     print('----------------');
     transactionEnvelopeXdrBase64 = transaction.toEnvelopeXdrBase64();
     print(transactionEnvelopeXdrBase64);
-    txrep =
-        TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
+    txrep = TxRep.fromTransactionEnvelopeXdrBase64(transactionEnvelopeXdrBase64);
     print(txrep);
   });
 }
