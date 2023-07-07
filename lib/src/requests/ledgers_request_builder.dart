d765bb074282b0b9010495b3b9a2bdfbf31724f3
Jop Middelkamp <middelkamp@live.nl>
2023-07-07 16:06:28 +0200

    Dismiss reopening EventSource when Stream is cancelled

--- lib/src/requests/ledgers_request_builder.dart
+++ lib/src/requests/ledgers_request_builder.dart
@@ -67,8 +67,10 @@ class LedgersRequestBuilder extends RequestBuilder {
       cancelled = true;
     };
     void createNewEventSource() {
-      Uri uri = this.buildUri();
-      EventSource.connect(uri).then((eventSource) {
+      if (cancelled) {
+        return;
+      }
+      EventSource.connect(this.buildUri()).then((eventSource) {
         eventSource.listen((Event event) {
           if (cancelled) {
             return null;
--- lib/src/requests/operations_request_builder.dart
+++ lib/src/requests/operations_request_builder.dart
@@ -109,8 +109,10 @@ class OperationsRequestBuilder extends RequestBuilder {
       cancelled = true;
     };
     void createNewEventSource() {
-      Uri uri = this.buildUri();
-      EventSource.connect(uri).then((eventSource) {
+      if (cancelled) {
+        return;
+      }
+      EventSource.connect(this.buildUri()).then((eventSource) {
         eventSource.listen((Event event) {
           if (cancelled) {
             return null;
